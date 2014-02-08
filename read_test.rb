98021c3d96e06bccda830c9deb77eb31b25ada17
Naoto Usuyama <usuyama.naoto@gmail.com>
2014-02-08 18:25:44 +0900

    Initial commit

---
+++ read_test.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require './sam_parser.rb'
+
+$data = []
+open "test.sam" do |f|
+  while l = f.gets
+    r = l.chomp.split("\t")
+    $data.push Read.new(r)
+  end
+end
+
+def should_include_variant(variants, type, ref, obs, left, right)
+  assert((variants.find_index {|v|
+    v.type == type and
+    (type == :soft or
+     (v.ref == ref and v.obs == obs)) and
+    v.left == left and
+    v.right == right
+  }), [variants, type, ref, obs, left, right].inspect)
+end
+
+class TestRead < MiniTest::Unit::TestCase
+  def test_basic
+    r = $data[0]
+    variants = r.get_variants
+    should_include_variant(variants, :mismatch, "A", "T", 1824, 1825)
+    should_include_variant(variants, :mismatch, "C", "A", 1846, 1847)
+  end
+
+  def test_del
+    r = $data[1]
+    variants = r.get_variants
+    should_include_variant(variants, :del, "GAC", "-", 1826, 1829)
+  end
+
+  def test_ins
+    r = $data[2]
+    variants = r.get_variants
+    should_include_variant(variants, :ins, "-", "GTC", 1328, 1328)
+    should_include_variant(variants, :mismatch, "T", "G", 1338, 1339)
+  end
+
+  def test_softclip
+    r = $data[3]
+    variants = r.get_variants
+    should_include_variant(variants, :soft, "GCAACAATGG", "T" * 10, 1368, 1378)
+    should_include_variant(variants, :mismatch, "C", "A", 1419, 1420)
+  end
+end
---
+++ sam_parser.rb
@@ -0,0 +1,166 @@
+#!/usr/bin/env ruby
+
+require 'pattern-match'
+
+class Variant
+  attr_accessor :type, :ref, :obs, :left, :right, :len
+
+  # init by giving a hash
+  def initialize(attributes={})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def inspect
+    "#{@type} #{@left}-#{@right} #{@ref} #{@obs} len:#{@len}"
+  end
+end
+
+class MD
+  attr_accessor :type, :len, :ref
+
+  def initialize(tag)
+    if tag[0] == "^"
+      @type = :del
+      @len = tag.length - 1
+      @ref = tag[1..-1]
+    elsif tag =~ /\d+/
+      @type = :match
+      @len = tag.to_i
+    else
+      @type = :mismatch
+      @len = 1
+      @ref = tag[0]
+    end
+  end
+end
+
+class Read
+  attr_accessor :seq_name, :flag, :chr, :pos, :map_quality, :cigar, :seq, :base_quality, :md, :left_most_pos
+
+  def initialize(r)
+    @seq_name, @flag, @chr, @pos, @map_quality, @cigar = r[0..5]
+    @pos = @pos.to_i
+    @seq = r[9]
+    @base_quality = r[10]
+    @md = r[12]
+
+    # pos does not include the soft-clipped bases
+    cigar = @cigar.scan(/\d+./)
+    if cigar[0][-1] == "S"
+      @left_most_pos = @pos - cigar[0][0..-2].to_i
+    else
+      @left_most_pos = @pos
+    end
+  end
+
+  def get_variants
+    md = @md.scan(/\d+|[A-Z]+|\^[A-Z]+/)[2..-1]
+    cigar = @cigar.scan(/\d+./)
+
+    # first, make variants from CIGAR
+    vars = []
+    for c in cigar
+      vars << Variant.new({:len => c[0..-2].to_i, :type =>
+        case c[-1]
+        when "M" then :match
+        when "I" then :ins
+        when "S" then :soft
+        when "D" then :del
+        else
+          raise "not_supported"
+        end
+      })
+    end
+
+    # prepare index for tracking CIGAR & MD
+    var_idx, m_idx = -1, -1
+    current_var, current_md = nil, nil
+    get_next_var = proc {
+      var_idx += 1
+      if var_idx < vars.length
+        current_var = vars[var_idx]
+      end
+    }
+    get_next_md = proc {
+      m_idx += 1
+      if m_idx < md.length
+        current_md = MD.new(md[m_idx])
+      end
+    }
+    # prepare the first pair of CIGAR and MD
+    get_next_var.call
+    get_next_md.call
+
+    @variants = []
+    while var_idx < vars.length and m_idx < md.length
+      match([current_var.type, current_md.type]) do
+        with _[:soft, _] {
+          @variants << current_var.clone
+          get_next_var.call
+        }
+        with _[_, :del] {
+          current_var.ref = current_md.ref
+          @variants << current_var.clone
+          get_next_var.call
+          get_next_md.call
+        }
+        with _[_, :mismatch] {
+          @variants << Variant.new({:type => :mismatch, :ref => current_md.ref, :len => 1})
+          current_var.len -= 1
+          get_next_md.call
+        }
+        with _[:ins, :match] {
+          @variants << current_var.clone
+          get_next_var.call
+        }
+        with _[:match, :match] {
+          if current_var.len > current_md.len
+            @variants << Variant.new({:type => :match, :len => current_md.len })
+            current_var.len -= current_md.len
+            get_next_md.call
+            get_next_var.call if current_var.len == 0
+          else
+            @variants << current_var.clone
+            current_md.len -= current_var.len
+            get_next_var.call
+            get_next_md.call if current_md.len == 0
+          end
+        }
+      end
+    end
+
+    # setting left & right pos for variants
+    k = @left_most_pos
+    for i in 0..@variants.length-1
+      v = @variants[i]
+      v.left = k - 1
+      if v.type == :ins
+        v.right = v.left
+      else #v.type == :mismatch, :ins, :soft, :match
+        k += v.len
+        v.right = v.left + v.len
+      end
+    end
+
+    # setting obs for variants
+    b = 0
+    for i in 0..@variants.length-1
+      v = @variants[i]
+      if v.type == :del
+        v.obs = "-"
+      else
+        case v.type
+        when :ins
+          v.ref = "-"
+        end
+        v.obs = seq[b, v.len]
+        b += v.len
+      end
+    end
+
+    @variants
+  end
+
+end
